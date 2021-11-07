2a43a873703e52a525d43b239b9338b684c5de6a
Lukas Wiklund <lukas@wiklund.se>
2021-11-07 13:51:24 +0100

    Implemented error handling

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import * as dotenv from "dotenv"
 import fetch from "node-fetch"
 import * as Avanza from "avanza"
 
-
 dotenv.config()
 
 const client = new Discord.Client()
@@ -16,26 +15,29 @@ function start() {
 	client.user?.setActivity({ name: "TO THE MOON 🚀", type: "COMPETING" })
 
 	updatedPrices().catch(error => console.error(error))
-	setInterval(() =>
-		updatedPrices().catch(error => console.error(error)),
-		10 * 60 * 1000
-	)
+	setInterval(() => updatedPrices().catch(error => console.error(error)), 10 * 60 * 1000)
 }
 
 async function updatedPrices() {
-	const response = await fetch(
-		"https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
-		{ headers: { "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY } }
-	)
+	const response = await fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest", {
+		headers: { "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY },
+	})
 	const data = await response.json()
-	const adaPrice = await data["data"].find((x: any) => x.id === 2010)["quote"]["USD"]["price"] as number
+	const adaPrice = (await data["data"].find((x: any) => x.id === 2010)["quote"]["USD"]["price"]) as number
 
 	const avanza = new Avanza()
-	await avanza.authenticate({
-		username: process.env.AVANZA_USERNAME,
-		password: process.env.AVANZA_PASSWORD,
-		totpSecret: process.env.AVANZA_TOTP_SECRET,
-	})
+
+	try {
+		await avanza.authenticate({
+			username: process.env.AVANZA_USERNAME,
+			password: process.env.AVANZA_PASSWORD,
+			totpSecret: process.env.AVANZA_TOTP_SECRET,
+		})
+	} catch (error) {
+		console.error("Failed to authenticate", error)
+		return
+	}
+
 	const omxs = await avanza.search("OMXSPI").then((data: any) => data.hits[0].topHits[0])
 	const global = await avanza.search("W1DOW").then((data: any) => data.hits[0].topHits[0])
 
